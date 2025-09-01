e7f74ff801ae174421c763bfb52c644e299a321a
Vaibhav Lohar <99500344+Vaibhav-888@users.noreply.github.com>
2025-09-01 09:41:15 +0000

    iterate_over_dict.py added

---
+++ iterate_over_dict.py
@@ -0,0 +1,15 @@
+my_dict = {
+    "role1": "description1",
+    "role2": "description2",
+    "role3": "description3",
+    "role4": "description4"
+}
+
+for key_values in my_dict.items():
+    print(key_values)
+
+for key in my_dict.keys():
+    print(key)
+
+for value in my_dict.values():
+    print(value)
